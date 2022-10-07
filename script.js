a70e3ccf2a181ae8539b2e6263c266893c699d17
hamo-o <yheel369@gmail.com>
2022-10-07 16:30:26 +0900

    feat: localStorage 구현

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ const doneList = document.querySelector('.done-box__list');
 let todoCount = 0;
 let doneCount = 0;
 
+let todos = [];
+let dones = [];
+
+const checkLocal = () => {
+  todos = JSON.parse(localStorage.getItem('todos'));
+  dones = JSON.parse(localStorage.getItem('dones'));
+  renderTodoList(todos);
+  renderDoneList(dones);
+};
+
 // input 체크하고 Todo 만들기
 const createTodo = (e) => {
   e.preventDefault();
@@ -13,7 +23,11 @@ const createTodo = (e) => {
   const todoInputVal = todoInput.value;
 
   if (todoInputVal) {
-    renderTodoList(todoInputVal);
+    // 입력값을 배열에 넣기
+    todos = [...todos, todoInputVal];
+    console.log(todos);
+    localStorage.setItem('todos', JSON.stringify(todos));
+    renderTodoList(todos);
   } else {
     todoInput.focus();
     alert('할 일을 입력해주세요!');
@@ -24,60 +38,65 @@ const createTodo = (e) => {
 myForm.addEventListener('submit', createTodo);
 
 // To Do 부분 렌더링
-const renderTodoList = (text) => {
-  const newTodo = document.createElement('li');
-  const newTodoSpan = document.createElement('span');
-  const todoDelBtn = document.createElement('i');
+const renderTodoList = (todos) => {
+  todos.forEach((it) => {
+    const newTodo = document.createElement('li');
+    const newTodoSpan = document.createElement('span');
+    const todoDelBtn = document.createElement('i');
 
-  // item text
-  newTodoSpan.innerText = text;
-  newTodoSpan.className = 'list-box__text';
-  newTodoSpan.addEventListener('click', toggleTodo);
+    // item text
+    newTodoSpan.innerText = it;
+    newTodoSpan.className = 'list-box__text';
+    newTodoSpan.addEventListener('click', toggleTodo);
 
-  // item 삭제버튼
-  todoDelBtn.className = 'list-box__btn fa-solid fa-circle-xmark';
-  todoDelBtn.addEventListener('click', delTodo);
+    // item 삭제버튼
+    todoDelBtn.className = 'list-box__btn fa-solid fa-circle-xmark';
+    todoDelBtn.addEventListener('click', delTodo);
 
-  // li 태그 안에 넣어주기
-  newTodo.className = 'list-box__item';
-  newTodo.append(newTodoSpan, todoDelBtn);
+    // li 태그 안에 넣어주기
+    newTodo.id = new Date().valueOf();
+    newTodo.className = 'list-box__item';
+    newTodo.append(newTodoSpan, todoDelBtn);
 
-  // ul 태그 안에 새로운 item 넣어주기
-  todoList.appendChild(newTodo);
+    // ul 태그 안에 새로운 item 넣어주기
+    todoList.appendChild(newTodo);
 
-  todoCount++;
+    todoCount++;
 
-  // input clear
-  document.querySelector('.input-box__input').value = '';
+    // input clear
+    document.querySelector('.input-box__input').value = '';
 
-  updateTodoCount();
+    updateTodoCount();
+  });
 };
 
 // Done 부분 렌더링
-const renderDoneList = (text) => {
-  const newDone = document.createElement('li');
-  const newDoneSpan = document.createElement('span');
-  const doneDelBtn = document.createElement('i');
+const renderDoneList = (dones) => {
+  dones.forEach((it) => {
+    const newDone = document.createElement('li');
+    const newDoneSpan = document.createElement('span');
+    const doneDelBtn = document.createElement('i');
 
-  // item text
-  newDoneSpan.innerText = text;
-  newDoneSpan.className = 'done-box__text';
-  newDoneSpan.addEventListener('click', toggleTodo);
+    // item text
+    newDoneSpan.innerText = it;
+    newDoneSpan.className = 'done-box__text';
+    newDoneSpan.addEventListener('click', toggleTodo);
 
-  // item 삭제버튼
-  doneDelBtn.className = 'done-box__btn fa-solid fa-circle-xmark';
-  doneDelBtn.addEventListener('click', delTodo);
+    // item 삭제버튼
+    doneDelBtn.className = 'done-box__btn fa-solid fa-circle-xmark';
+    doneDelBtn.addEventListener('click', delTodo);
 
-  // li 태그 안에 넣어주기
-  newDone.className = 'done-box__item';
-  newDone.append(newDoneSpan, doneDelBtn);
+    // li 태그 안에 넣어주기
+    newDone.className = 'done-box__item';
+    newDone.append(newDoneSpan, doneDelBtn);
 
-  // ul 태그 안에 새로운 item 넣어주기
-  doneList.appendChild(newDone);
+    // ul 태그 안에 새로운 item 넣어주기
+    doneList.appendChild(newDone);
 
-  doneCount++;
+    doneCount++;
 
-  updateDoneCount();
+    updateDoneCount();
+  });
 };
 
 const updateTodoCount = () => {
@@ -93,6 +112,14 @@ const updateDoneCount = () => {
 // item 삭제
 const delTodo = (e) => {
   const target = e.target.parentNode;
+
+  // todos = todos.filter((todo) => {
+  //   console.log(todo.id);
+  //   todo.id !== parseInt(target.id);
+  // });
+
+  // localStorage.setItem('todos', JSON.stringify(todos));
+  // console.log('삭제후 : ', todos);
 
   if (target.className === 'list-box__item') {
     target.remove();
@@ -111,9 +138,14 @@ const toggleTodo = (e) => {
 
   if (target.className === 'list-box__item') {
     delTodo(e);
-    renderDoneList(e.target.textContent);
+    dones = [...dones, e.target.textContent];
+    console.log(dones);
+    localStorage.setItem('dones', JSON.stringify(dones));
+    renderDoneList(dones);
   } else {
     delTodo(e);
+    todos = [...todos, e.target.textContent];
+    localStorage.setItem('todos', JSON.stringify(todos));
     renderTodoList(e.target.textContent);
   }
 };
